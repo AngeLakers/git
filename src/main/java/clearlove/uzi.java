31f4bb6c88e8fa6e465edcab9adf0d0ee0de105d
shinyruo <shinyruo520@gmail.com>
2023-03-04 16:04:12 +1100

    imp first commit

--- src/main/java/clearlove/uzi.java
+++ src/main/java/clearlove/uzi.java
@@ -5,6 +5,6 @@ public class uzi {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!3");
-
+        System.out.println("Hello world!4");
     }
 }
