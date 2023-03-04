0e38480327cdd73365dce0ec596b98f5d4e37f6a
shinyruo <shinyruo520@gmail.com>
2023-03-04 15:23:43 +1100

    second commit

--- src/main/java/clearlove/uzi.java
+++ src/main/java/clearlove/uzi.java
@@ -3,6 +3,7 @@ package clearlove;
 public class uzi {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello world!");
 
     }
 }
